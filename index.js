cca9dc93450494ce23258f3067ba3a8b109ab037
Pattragun005 <s5830213005@phuket.psu.ac.th>
2017-10-01 22:07:46 +0700

    Almost complete!

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 $(function () {
-
-    $("#loaddata").click(function () {
-
-        $.get("data.json", function (data) {
-            console.log(data);
-            $.each(data, function (index, item) {
-                var row = "<h3>" + data.weather[0].description + "</h3>";
-                $("#datapanel").append(row);
-            });
-
-        });
-    });
-
     $("#loadwebapi").click(function () {
-
-        $("#wpanel").empty();
-
         var city = $("#city").val();
-
-        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city;
-        url = "&APPID=4c32373ac55eb2cecd27c83ac44bc488";
-
+        console.log(city);
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=4c32373ac55eb2cecd27c83ac44bc488";
         $.get(url, function (data) {
             console.log(data);
-            var row = "<h3>" + data.weather[0].description + "</h3>";
-            row += "<h3>" + data.main.temp/10 + "C</h3>";
-            $("#wpanel").append(row);
-
-
+            var show = "<h2>"+data.name+"</h2><p>"+data.main.temp+"</p><p>"+data.weather[0].description+"</p><p>Humidity : "+data.main.humidity+"%</p><p>Presure : "+data.main.pressure+" hPa</p>";
+           console.log(show);
+            $("#wpanel").append(show);
         });
     });
 });
